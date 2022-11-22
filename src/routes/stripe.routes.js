a2f18f71560851c75a5719162b07bb119ac1e589
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2022-11-22 08:14:13 +0700

    Update: add express raw on stripe controller

--- src/routes/stripe.routes.js
+++ src/routes/stripe.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const express = require('express');
 const { webhook } = require('../controllers/stripe.controller');
 
-router.post('/', webhook);
+router.post('/', express.raw({ type: 'application/json' }), webhook);
 
 module.exports = router;
