4bcd10c3f10ce87b97c7ec8d68f493bc3041c6e7
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2022-11-23 07:26:40 +0700

    Update: add ban count crud

--- src/controllers/simulator-option-ban.controller.js
+++ src/controllers/simulator-option-ban.controller.js
@@ -1,15 +1,35 @@
 const NotFoundError = require('../exceptions/NotFoundError');
-const { SimulatorOptionBans } = require('../models');
+const { SimulatorOptionBans, Sequelize } = require('../models');
 const { createSuccessResponse } = require('../utils/response');
 
 exports.findAll = async (req, res, next) => {
   try {
+    const { limit, offset } = req.query;
+    const params = {};
+    if (limit) params.limit = +limit;
+    if (offset) params.offset = +offset - 1;
     const data = await SimulatorOptionBans.findAll();
-    return createSuccessResponse(
-      res,
-      'Success get simulator option bans',
+    const response = {
+      status: 'success',
+      message: 'Success get ban amount',
       data,
-    );
+    };
+    delete params.limit;
+    delete params.offset;
+    if (limit) {
+      const totalItems = await SimulatorOptionBans.findAll({
+        attributes: [[Sequelize.fn('COUNT', Sequelize.col('id')), 'total']],
+      });
+
+      const page = {
+        totalItems: parseInt(totalItems[0]?.dataValues?.total || 0, 10),
+        limit: +limit,
+        page: +offset,
+      };
+      response.page = page;
+    }
+
+    return res.json(response);
   } catch (error) {
     return next(error);
   }
@@ -49,7 +69,7 @@ exports.update = async (req, res, next) => {
   try {
     const { id } = req.params;
     const params = {
-      ban_count: req.params.ban_count,
+      ban_count: req.body.ban_count,
     };
     const item = await SimulatorOptionBans.findByPk(id);
     if (!item) {
--- src/routes/simulator-option-ban.routes.js
+++ src/routes/simulator-option-ban.routes.js
@@ -5,19 +5,22 @@ const {
   simulatorOptionBanPostSchema,
   simulatorOptionBanPutSchema,
 } = require('../validators/simulator-option-ban.validator');
+const auth = require('../middleware/auth');
 
 router.get('/', simulatorOptionBanController.findAll);
 router.post(
   '/',
+  auth(),
   Validator(simulatorOptionBanPostSchema),
   simulatorOptionBanController.create,
 );
 router.put(
   '/:id',
+  auth(),
   Validator(simulatorOptionBanPutSchema),
   simulatorOptionBanController.update,
 );
-router.get('/:id', simulatorOptionBanController.findById);
-router.delete('/:id', simulatorOptionBanController.delete);
+router.get('/:id', auth(), simulatorOptionBanController.findById);
+router.delete('/:id', auth(), simulatorOptionBanController.delete);
 
 module.exports = router;
