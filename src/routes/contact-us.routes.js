662344f71d4ff2d80b2bf93f1c6e28424b22e1ba
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2023-01-31 06:45:31 +0700

    Update: add auth middleware to contact us route

--- src/routes/contact-us.routes.js
+++ src/routes/contact-us.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const auth = require('../middleware/auth');
 const controller = require('../controllers/contact-us.controller');
 const Validator = require('../middleware/validator');
 const { contactUsPostSchema } = require('../validators/contact-us.validator');
 
-router.post('/', Validator(contactUsPostSchema), controller.sendMail);
+router.post('/', auth(), Validator(contactUsPostSchema), controller.sendMail);
 
 module.exports = router;
